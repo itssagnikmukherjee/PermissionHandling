dca2cebf1f89c7bb5cd66175512584d67b6d5f9e
itssagnikmukherjee <mukherjee.sagnik17@gmail.com>
2024-09-22 23:24:43 +0530

    permission button added

--- app/src/main/java/app/sagnikmukherjee/permissionhandling/MainActivity.kt
+++ app/src/main/java/app/sagnikmukherjee/permissionhandling/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,28 +21,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             PermissionHandlingTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Button(onClick = {}) {
+                        Text("Access Permissions")
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PermissionHandlingTheme {
-        Greeting("Android")
     }
 }
