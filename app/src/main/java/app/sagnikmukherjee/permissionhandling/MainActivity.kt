3576f67fe524a53b3fc28f4ec0342a997f16e479
itssagnikmukherjee <mukherjee.sagnik17@gmail.com>
2024-09-23 20:44:28 +0530

    permission API ++

--- app/src/main/java/app/sagnikmukherjee/permissionhandling/MainActivity.kt
+++ app/src/main/java/app/sagnikmukherjee/permissionhandling/MainActivity.kt
@@ -80,7 +80,6 @@ fun Test() {
 @Composable
 private fun FeatureThatRequiresCameraPermission() {
 
-    // Camera permission state
     val cameraPermissionState = rememberPermissionState(
         android.Manifest.permission.CAMERA
     )
@@ -90,13 +89,8 @@ private fun FeatureThatRequiresCameraPermission() {
     } else {
         Column {
             val textToShow = if (cameraPermissionState.status.shouldShowRationale) {
-                // If the user has denied the permission but the rationale can be shown,
-                // then gently explain why the app requires this permission
                 "The camera is important for this app. Please grant the permission."
             } else {
-                // If it's the first time the user lands on this feature, or the user
-                // doesn't want to be asked again for this permission, explain that the
-                // permission is required
                 "Camera permission required for this feature to be available. " +
                         "Please grant the permission"
             }
