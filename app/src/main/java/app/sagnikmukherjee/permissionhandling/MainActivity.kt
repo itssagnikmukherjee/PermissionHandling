536806bc793c7141f034659963964a3a32c05069
itssagnikmukherjee <mukherjee.sagnik17@gmail.com>
2024-09-23 19:38:09 +0530

    accessing camera

--- app/src/main/java/app/sagnikmukherjee/permissionhandling/MainActivity.kt
+++ app/src/main/java/app/sagnikmukherjee/permissionhandling/MainActivity.kt
@@ -2,16 +2,27 @@ package app.sagnikmukherjee.permissionhandling
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import app.sagnikmukherjee.permissionhandling.ui.theme.PermissionHandlingTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,10 +32,36 @@ class MainActivity : ComponentActivity() {
         setContent {
             PermissionHandlingTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Button(onClick = {}) {
-                        Text("Access Permissions")
-                    }
+                    test()
                 }
+            }
+        }
+    }
+}
+
+@Composable
+fun test() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        val permission = android.Manifest.permission.CAMERA
+        var isGranted by remember { mutableStateOf(false) }
+        val launch = rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.RequestPermission(), onResult = {
+                isGranted = it
+            }
+        )
+        if (isGranted) {
+            Text("Camera Permission granted")
+        } else {
+            Button(
+                onClick = {
+                    launch.launch(permission)
+                }
+            ) {
+                Text("Get Permission")
             }
         }
     }
