e581a7f577413ae6be736bb6fcd66d6885ec2420
itssagnikmukherjee <mukherjee.sagnik17@gmail.com>
2024-09-23 20:40:34 +0530

    permission API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ android {
 
 dependencies {
 
+    implementation("com.google.accompanist:accompanist-permissions:0.31.2-alpha")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
